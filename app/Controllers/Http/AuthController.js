10ea843a2ae2621c0f17842eb10a2b8c187f77ec
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-07-06 00:04:16 -0300

    criando uma validação no cadastro de usuários

--- app/Controllers/Http/AuthController.js
+++ app/Controllers/Http/AuthController.js
@@ -1,14 +1,42 @@
 'use strict'
 
+const { validate } = use('Validator')
 const User = use('App/Models/User')
+const Database = use('Database')
+
 
 class AuthController {
     async register({ request }){
+
         const data = request.only(['username', 'email', 'password'])
 
-        const user = await User.create(data)
+        const rules = {
+            username: 'required',
+            email: 'required|email|unique:users,email',
+            password: 'required'
+        }
 
-        return user
+        const check_username = await Database.from('Users').where({
+            username: data.username
+        }).getCount()
+
+        const check_email = await Database.from('Users').where({
+            email: data.email
+        }).getCount()
+
+        if(check_email > 0 || check_username > 0){
+            return {
+                error: 'Usuário ou Email já cadastrado !',
+            }
+        }else{
+            const validation = await validate( data , rules)
+            if(validation.fails()){
+                return { error: validation.messages()}
+            }else{
+                const user = await User.create(data)
+                return user
+            }
+        }
     }
 
     async authenticate({ request, auth }){
