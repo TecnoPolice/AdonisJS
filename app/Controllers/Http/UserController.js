3c3307842354a93fe9c6df5cf4fcf469d7437d1f
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-07-10 15:23:34 -0300

    a verificação de usuário logado é feita antes da consulta agora

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -2,15 +2,18 @@
 const user = use('App/Models/User')
 const Database = use('Database')
 class UserController {
-
+    // Fução que mostra as informações de um usuário escolhido.
     async show({ auth, params }){
+      try {
+        // Verificando se o usuário está autenticado.
+        await auth.check()
+      } catch (error) {
+        return {message: 'Você não está autenticado !'}
+      }
+      // Buscando no banco de dados o usuário que tem o ID informado pela URL.
       const users = await Database.from('users').where('id', params.id)
-        try {
-            await auth.check()
-            return {user: users}
-          } catch (error) {
-            return {message: 'Você não está autenticado !'}
-          }
+      // Retornando o usuário informado.
+      return {user: users}
     }
 }
 
